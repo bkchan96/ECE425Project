52f51a2f09d7ea5db67979757031ff9f4d8574c4
bkchan96 <39748671+bkchan96@users.noreply.github.com>
2018-07-04 15:07:23 -0700

    Updated 3 7/4/2018 3:07PM

--- ProjectPart2/ProjectPart2.srcs/sources_1/new/REG_FILE_16x16.v
+++ ProjectPart2/ProjectPart2.srcs/sources_1/new/REG_FILE_16x16.v
@@ -24,6 +24,10 @@ module REG_FILE_16x16(WADR, WEN, clk, RADR1, RADR2, OUT1, OUT2, ALUIN);
     begin
         if (WEN == 1)
             REGFILE[WADR] = ALUIN; //Write operation when WEN == 1
+    end
+    
+    always @ (*)
+    begin
         OUT1 = REGFILE[RADR1]; //Read from requested register
         OUT2 = REGFILE[RADR2]; //Read from requested register
     end
